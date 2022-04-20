adf60377c83abc1c16d9efc7dc1eca6264b08f50
ramumaha <62795822+ramumaha@users.noreply.github.com>
2022-04-20 21:39:16 +0530

    added slider component

--- MovieRush/src/app/home/home.component.ts
+++ MovieRush/src/app/home/home.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+interface carouseImages{
+  imageSrc:string;
+  imageAlt:string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +11,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  @Input() images:carouseImages[]=[];
+  @Input() indicators=true;
+  @Input() controls=true;
+  @Input() autoSlide=false;
+  @Input() slideInterval=3000;
+  selectedIndex=0;
   constructor() { }
 
   ngOnInit(): void {
+    if(this.autoSlide){
+      this.autoSlideImages();
+
+    }
   }
+  autoSlideImages():void{
+    setInterval(()=>{
+      this.onNextClick();
+    },this.slideInterval);
+
+  }
+
+  selectImage(index:number):void{
+    this.selectedIndex=index;
+    }
+
+    onPrevClick():void{
+      if(this.selectedIndex===0){
+       this.selectedIndex=this.images.length-1;
+      }else{
+        this.selectedIndex--;
+      }
+    }
+
+    onNextClick():void{
+      if(this.selectedIndex==this.images.length-1){
+        this.selectedIndex=0;
+      }else{
+        this.selectedIndex++;
+      }
+    }
+
+   
 
 }
