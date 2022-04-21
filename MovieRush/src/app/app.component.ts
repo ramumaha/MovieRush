f6c081fe38d9b01c8a46f4b7b8195b913188b3c9
ramumaha <62795822+ramumaha@users.noreply.github.com>
2022-04-22 00:27:56 +0530

    revert

--- MovieRush/src/app/app.component.ts
+++ MovieRush/src/app/app.component.ts
@@ -16,23 +16,23 @@ export class AppComponent {
   images = [
     {
       imageSrc:
-        'https://i.pinimg.com/originals/3c/f4/cf/3cf4cf74674ede94488ee71d7ae04ade.jpg',
-      imageAlt: 'tintin',
+        'https://images.unsplash.com/photo-1460627390041-532a28402358?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'nature1',
     },
     {
       imageSrc:
-        'https://i.pinimg.com/originals/42/75/01/42750115d68a876558e7e0115b6600c8.jpg',
-      imageAlt: 'tranquility of blood',
+        'https://images.unsplash.com/photo-1470252649378-9c29740c9fa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'nature2',
     },
     {
       imageSrc:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3oDSiKS8_1wGsxS-cmj6IKu6L8LK5WlfGvg&usqp=CAU',
-      imageAlt: 'inside out',
+        'https://images.unsplash.com/photo-1640844444545-66e19eb6f549?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80',
+      imageAlt: 'person1',
     },
     {
       imageSrc:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgudGb7qYL3AEFWVbEdBZ1aGSw7J-V_kqqYg&usqp=CAU',
-      imageAlt: 'hunger games',
+        'https://images.unsplash.com/photo-1490730141103-6cac27aaab94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      imageAlt: 'person2',
     },
   ]
 }
--- MovieRush/src/app/movies/movie-list/movie-list.component.ts
+++ MovieRush/src/app/movies/movie-list/movie-list.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit ,Input} from '@angular/core';
 
-<<<<<<< HEAD
+
 
 interface moviePoster{
   imageSrc:string;
   imageAlt:string;
 
 }
-=======
+
 import { Movie } from '../../shared/movie.model';
->>>>>>> parent of 3ef8905 (side by side movie display)
+
 
 @Component({
   selector: 'app-movie-list',
