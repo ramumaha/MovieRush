9ce1a32711db5a938faadb7d9029362ad2f11bea
ramumaha <62795822+ramumaha@users.noreply.github.com>
2022-04-22 00:20:40 +0530

    changes

--- MovieRush/src/app/movies/movie-list/movie-list.component.ts
+++ MovieRush/src/app/movies/movie-list/movie-list.component.ts
@@ -12,16 +12,14 @@ interface moviePoster{
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
+  @Input() movies:Movie[]=[
+    new Movie('Test','this is test','https://assetscdn1.paytm.com/images/cinema/KGF-1035x420-c3374de0-b6ff-11ec-9639-8322852eadd4.jpg','action')
 
-  @Input() posters:moviePoster[]=[];
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onClicked():void{
-
   }
 
 }
