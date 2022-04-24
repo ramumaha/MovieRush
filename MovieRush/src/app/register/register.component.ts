2ebeb452674cd856632a358e419fe597275da489
ramumaha <62795822+ramumaha@users.noreply.github.com>
2022-04-25 01:16:36 +0530

    added sign in and movie card kind

--- MovieRush/src/app/register/register.component.ts
+++ MovieRush/src/app/register/register.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+   sign_in_btn = document.querySelector("#sign-in-btn");
+ sign_up_btn = document.querySelector("#sign-up-btn");
+ container = document.querySelector(".container");
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  removeClass(){
+    this.container.classList.remove("sign-up-mode");
+  }
+  addClass(){
+    this.container.classList.add("sign-up-mode");
+
+
+  }
+
 
 }
